555e6d9af522a5ccbb66816bcf158b3c31aa146f
Sergei Rakitin <sergei_r_qschool@qsoft.ru>
2022-07-05 12:41:57 +0400

    first commit

---
+++ routes/web.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::group(['namespace' => 'Main'], function () {
+    Route::get('/', IndexController::class);
+});
+
+Route::group(['namespace' => 'Admin', 'prefix' => 'admin'], function () {
+    Route::group(['namespace' => 'Main'], function () {
+        Route::get('/', IndexController::class);
+    });
+
+    Route::group(['namespace' => 'Post', 'prefix' => 'posts'], function () {
+        Route::get('/', IndexController::class)->name('admin.post.index');
+        Route::get('/create', CreateController::class)->name('admin.post.create');
+        Route::post('/', StoreController::class)->name('admin.post.store');
+        Route::get('/{post}', ShowController::class)->name('admin.post.show');
+        Route::get('/{post}/edit', EditController::class)->name('admin.post.edit');
+        Route::patch('/{post}', UpdateController::class)->name('admin.post.update');
+        Route::delete('/{post}', DeleteController::class)->name('admin.post.delete');
+    });
+
+    Route::group(['namespace' => 'Category', 'prefix' => 'categories'], function () {
+        Route::get('/', IndexController::class)->name('admin.category.index');
+        Route::get('/create', CreateController::class)->name('admin.category.create');
+        Route::post('/', StoreController::class)->name('admin.category.store');
+        Route::get('/{category}', ShowController::class)->name('admin.category.show');
+        Route::get('/{category}/edit', EditController::class)->name('admin.category.edit');
+        Route::patch('/{category}', UpdateController::class)->name('admin.category.update');
+        Route::delete('/{category}', DeleteController::class)->name('admin.category.delete');
+    });
+
+    Route::group(['namespace' => 'Tag', 'prefix' => 'tags'], function () {
+        Route::get('/', IndexController::class)->name('admin.tag.index');
+        Route::get('/create', CreateController::class)->name('admin.tag.create');
+        Route::post('/', StoreController::class)->name('admin.tag.store');
+        Route::get('/{tag}', ShowController::class)->name('admin.tag.show');
+        Route::get('/{tag}/edit', EditController::class)->name('admin.tag.edit');
+        Route::patch('/{tag}', UpdateController::class)->name('admin.tag.update');
+        Route::delete('/{tag}', DeleteController::class)->name('admin.tag.delete');
+    });
+});
+
+Auth::routes();
+
